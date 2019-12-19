fa6acb4890d4b068fcddb2d5b6d500d0e8b0b223
Aylin Demir <aylin.de@outlook.com>
2019-12-19 00:24:56 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,12 +1,6 @@
-check1="You are a teenager!"
-check2="you are not a teenager"
-check3="you are a kid"
-
 function basicTeenager(age) {
-if(13<age<19){
+if(13<=age<=19){
   return "You are a teenager!";
-}else{
-  return "You are not a teenager";
 }
 }
 
